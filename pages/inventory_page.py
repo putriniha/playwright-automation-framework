db7c928704742ef57353398ac47a9536a5a2a4f1
putriniha <putrinihayatul7@gmail.com>
2026-01-09 13:23:25 +0700

    Update Inventory Page and Add Item to Cart

--- pages/inventory_page.py
+++ pages/inventory_page.py
@@ -3,9 +3,14 @@ class InventoryPage:
         self.page = page
         self.add_to_cart_button = "#add-to-cart-sauce-labs-backpack"
         self.cart_icon = ".shopping_cart_link"
-
+    
+    def is_loaded(self):
+        return "inventory" in self.page.url
+    
     def add_item_to_cart(self):
         self.page.click(self.add_to_cart_button)
-    
+        
     def go_to_cart(self):
         self.page.click(self.cart_icon)
+    
+    
