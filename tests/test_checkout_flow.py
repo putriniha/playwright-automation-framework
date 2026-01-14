9d051afd75da6cb406024de7d51ba03768dd18e9
putriniha <putrinihayatul7@gmail.com>
2026-01-14 11:13:13 +0700

    Add parametrized tests for checkout flows

--- tests/test_checkout_flow.py
+++ tests/test_checkout_flow.py
@@ -1,3 +1,5 @@
+import pytest
+
 def test_user_can_checkout_successfully(
     login_page,
     inventory_page,
@@ -11,13 +13,46 @@ def test_user_can_checkout_successfully(
     inventory_page.add_item_to_cart()
     inventory_page.go_to_cart()
 
-    # cart_page.update_quantity("2")
+    # cart_page.update_quantity(2)
 
     assert cart_page.is_item_visible()
     
     cart_page.proceed_to_checkout()
 
     checkout_page.fill_information("Putri", "QA", "12345")
+
+    confirmation = checkout_complete_page.get_confirmation_text()
+    assert "Thank you for your order!" in confirmation
+
+@pytest.mark.parametrize(
+    "first_name, last_name, postal_code",
+    [
+        ("Test", "User", "12345"),
+        ("Putri", "QA", "12345"),
+    ]
+)
+
+def test_checkout_with_various_user_info(
+    login_page,
+    inventory_page,
+    cart_page,
+    checkout_page,
+    checkout_complete_page,
+    first_name,
+    last_name,
+    postal_code
+):
+    login_page.open()
+    login_page.login("standard_user", "secret_sauce")
+
+    inventory_page.add_item_to_cart()
+    inventory_page.go_to_cart()
+
+    assert cart_page.is_item_visible()
+    
+    cart_page.proceed_to_checkout()
+
+    checkout_page.fill_information(first_name, last_name, postal_code)
 
     confirmation = checkout_complete_page.get_confirmation_text()
     assert "Thank you for your order!" in confirmation
