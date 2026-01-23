1d7814e5137f4cd046f060beb33ef0d3bbb046d5
putriniha <putrinihayatul7@gmail.com>
2026-01-23 11:17:20 +0700

    update: add smoke test

--- tests/test_login_positive.py
+++ tests/test_login_positive.py
@@ -1,13 +1,11 @@
+import pytest
 from pages.login_page import LoginPage
 from pages.inventory_page import InventoryPage
 
+@pytest.mark.smoke
+def test_user_can_login_successfully(Login_page, Inventory_page):
+    Login_page.open()
+    Login_page.login("standard_user", "secret_sauce")
 
-def test_login_happy_path(page):
-    login_page = LoginPage(page)
-    inventory_page = InventoryPage(page)
-
-    login_page.open()
-    login_page.login("standard_user", "secret_sauce")
-    
-    assert inventory_page.is_loaded()
+    assert Inventory_page.is_loaded()
     
